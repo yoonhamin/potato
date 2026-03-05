fee35c530e8f769f8bbf9374d8f3fcfa98216fe8
shavrinas <ynhamin@gmail.com>
2026-03-05 21:59:37 +0900

    기능 정의 (func) f_alarm 에 playAlarm() 함수 개선 --------------------------------- playAlarm() 함수
      이름 : playAlarm()
      기능 : 설정한 시간(5초) 동안 소리와 진동을 울림니다.
      목적 : 작업자에게 위험을 알림니다.
    ---------------------------------
    assets/sounds/alarm.mp3 가 추가되었습니다.
    출처 alarm.txt
    ---------------------------------
    pubspec.yaml에 assets/sounds/ 등록
    flutter:
      assets:
        - assets/sounds/

--- lib/func/f_alarm.dart
+++ lib/func/f_alarm.dart
@@ -6,39 +6,49 @@ import 'dart:async';
 playAlarm() 함수
 
   이름 : playAlarm()
-  기능 : 5초간 소리와 진동을 울림니다.
+  기능 : 설정한 시간(5초) 동안 소리와 진동을 울림니다.
   목적 : 작업자에게 위험을 알림니다.
 
 */
 
 void playAlarm() async {
+  // ========================================
+  // init
+
+  int durTime = 5; // 알람 시간 (초)
+  final AudioPlayer audioPlayer = AudioPlayer();
 
   print("[F01Alarm01] : 알람 울림 작동");
 
   // ========================================
-  // 소리
+  // 소리 반복
 
-  final AudioPlayer audioPlayer = AudioPlayer();
+  await audioPlayer.setReleaseMode(ReleaseMode.loop);
 
   await audioPlayer.play(
-      UrlSource('https://www.soundjay.com/buttons/beep-01a.mp3')
+    AssetSource('sounds/alarm.mp3'),
   );
-  // 에셋에 파일이 없으면 인터넷 URL로 대체 < 즉 나중에 음원 리소스  추가하자
 
   // ========================================
-  // 진동
+  // 진동 반복
 
   if (await Vibration.hasVibrator() ?? false) {
     Vibration.vibrate(
-      pattern: [0, 500, 500, 500, 500, 500],
+      pattern: [0, 500, 500],
+      repeat: 0,
     );
   }
 
   // ========================================
-  // 시간
+  // 지속 시간
 
-  await Future.delayed(const Duration(seconds: 5));
+  await Future.delayed(Duration(seconds: durTime));
+
+  // ========================================
+  // 종료
+
   await audioPlayer.stop();
+  Vibration.cancel();
 
   print("[F01Alarm02] : 알람 울림 종료");
 }
